d5758019dba477c015236a4c7715b45ce4583dfc
victor291201 <victor291201hugo@gmail.com>
2023-01-25 16:41:15 -0500

    utilization of animations in login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -5,11 +5,47 @@ import "../styles/LogIn/styles.css";
 
 class LogIn extends Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state={
+            img: 1
+        };
+    }
+    componentDidMount(){
+        this.timerId = setInterval(()=>{
+            if(this.state.img <4){
+                this.setState(prevState => ({
+                    img: prevState.img +1
+                }));
+            }
+            else{
+                this.setState(prevState => ({
+                    img: 1
+                }));
+            }
+        },3000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerId)
     }
     render(){
         return(
-            <div className='Lgbody'>
+            <div className="Lgbody">
+                {
+                    {
+                        1:  <div className='Lgimg'>
+                                <div className='img1'></div><div className='img2 Lgdisapear'></div><div className='img3 Lgdisapear'></div><div className='img4 Lgdisapear'></div>
+                            </div>,
+                        2:  <div className='Lgimg'>
+                                <div className='img1 Lgdisapear'></div><div className='img2'></div><div className='img3 Lgdisapear'></div><div className='img4 Lgdisapear'></div>
+                            </div>,
+                        3:  <div className='Lgimg'>
+                                <div className='img1 Lgdisapear'></div><div className='img2 Lgdisapear'></div><div className='img3'></div><div className='img4 Lgdisapear'></div>
+                            </div>,
+                        4:  <div className='Lgimg'>
+                                <div className='img1 Lgdisapear'></div><div className='img2 Lgdisapear'></div><div className='img3 Lgdisapear'></div><div className='img4'></div>
+                            </div>,
+                    }[this.state.img]
+                }
                 <div className='Lg-pt1'>
                     <h1>Funimag</h1>
                     <p>¡La nueva red social hecha por estudiantes de la universidad del magdalena para estudiantes de la universidad del magdalena!</p>
